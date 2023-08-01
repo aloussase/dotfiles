95309366f898df366d8127b718945a9673efab76
Alexander Goussas <goussasalexander@gmail.com>
2023-07-31 19:03:17 -0500

    config fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,8 +5,11 @@ rvm default
 rvm use 3.1.0  >/dev/null
 nvm use 16     >/dev/null
 
-export FLYCTL_INSTALL="/home/aloussase/.fly"
+# Vi mode
+fish_vi_key_bindings
+function fish_mode_prompt; end
+set fish_cursor_default block blink
+set fish_cursor_insert line blink
 
-# >>> coursier install directory >>>
+# Some coursier thing
 set -gx PATH "$PATH:/home/aloussase/.local/share/coursier/bin"
-# <<< coursier install directory <<<

eabd35b7aef7d3c6ae11c355b79c6027b9e702c1
Alexander Goussas <goussasalexander@gmail.com>
2023-08-05 13:05:17 -0500

    nvim: Add highlight on yank autocmd

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,50 +1,7 @@
-vim.opt.swapfile      = false
-vim.opt.backup        = false
+require("options");
+require("autocmds");
 
-vim.opt.backspace     = "indent,eol,start"
-vim.opt.number        = true
-vim.opt.tabstop       = 4
-vim.opt.softtabstop   = 4
-vim.opt.shiftwidth    = 4
-vim.opt.shiftround    = true
-vim.opt.expandtab     = true
-
-vim.opt.laststatus    = 0
-vim.opt.background    = "dark"
-vim.opt.termguicolors = true
-vim.opt.cursorline    = true
-
-vim.opt.colorcolumn   = "80,100"
-
-vim.opt.hlsearch      = true
-vim.opt.ignorecase    = true
-vim.opt.incsearch     = true
-vim.opt.smartcase     = true
-
-vim.opt.wildmenu      = true
-vim.opt.wildmode      = "longest,list,full"
-
-vim.opt.splitright    = true
-vim.opt.splitbelow    = true
-
-vim.opt.list          = true
-vim.opt.listchars     = "eol:$"
-
-vim.opt.autoindent    = true
-vim.opt.smartindent   = true
-
-vim.opt.mouse         = "a"
--- vim.opt.visualbell
-vim.opt.signcolumn    = "yes"
-
-vim.opt.hidden        = true
-
-vim.opt.guifont       = "Hasklug Nerd Font Mono 12"
-
-vim.opt.wrap          = false
-vim.opt.viewdir       = "/tmp/view/"
-
-local lazypath        = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",

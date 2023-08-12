2404abffa8d35056b2a2ce7628f956aa8b94587a
Alexander Goussas <aloussase@debian-BULLSEYE-live-builder-AMD64>
2023-08-12 10:33:04 -0500

    nvim: Add textcase plugin

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -1,5 +1,15 @@
 return {
   {
+    'johmsalas/text-case.nvim',
+    config = function()
+      require('textcase').setup({})
+      require('telescope').load_extension('textcase')
+      vim.keymap.set('n', 'ga.', "<Cmd>Telescope textcase normal_mode theme=dropdown<CR>", {})
+      vim.keymap.set('n', 'lga.', "<Cmd>Telescope textcase normal_mode_lsp_change theme=dropdown<CR>", {})
+      vim.keymap.set('v', 'ga.', "<Cmd>Telescope textcase visual_mode theme=dropdown<CR>", {})
+    end
+  },
+  {
     'hrsh7th/nvim-cmp',
     dependencies = {
       -- Completion symbols

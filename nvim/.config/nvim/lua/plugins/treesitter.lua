6f8358de3eb02a38b570f30b9a98818ccee872f0
Alexander Goussas <goussasalexander@gmail.com>
2023-08-13 13:16:24 -0500

    add autotag plugin

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
       'nvim-treesitter/playground',
       'RRethy/nvim-treesitter-endwise',
+      'windwp/nvim-ts-autotag',
       'abecodes/tabout.nvim',
     },
     config = function()
@@ -11,7 +12,17 @@ return {
         ensure_installed = { "haskell", "lua", "cpp", "rust" },
         highlight = { enable = true },
         playground = { enable = true },
+        query_linter = {
+          enable = true,
+          use_virtual_text = true,
+        },
         endwise = { enable = true },
+        autotag = {
+          enable = true,
+          enable_rename = true,
+          enable_close = true,
+          enable_close_on_slash = true,
+        }
       })
 
       require('tabout').setup({})

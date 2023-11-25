95a9db6736f9057c95b280a1d7c538d4dbe44f95
Alexander Goussas <goussasalexander@gmail.com>
2023-11-25 10:33:23 -0500

    Update colors to use haskell theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,18 @@
 return {
   {
     "rebelot/kanagawa.nvim",
-    config = function()
-    end,
+    config = function() end,
   },
   {
     'yorickpeterse/nvim-grey',
-    config = function()
-    end
+    config = function() end
+  },
+  {
+    'marko-cerovac/material.nvim',
+    config = function() vim.cmd("colorscheme material-darker") end
+  },
+  {
+    'olimorris/onedarkpro.nvim',
+    config = function() end
   }
 }

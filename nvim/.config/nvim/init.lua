e93e6c4e7246f6c88c1dcf5ad0ac0ea450cd2a56
Alexander Goussas <goussasalexander@gmail.com>
2024-12-27 14:28:09 -0500

    add noice plugin nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -123,6 +123,25 @@ require("lazy").setup({
           end,
         })
       end
+    },
+    {
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+      },
+      config = function() 
+        require("noice").setup({
+          presets = {
+            bottom_search = true,
+            command_palette = true,
+            long_message_to_split = true,
+            inc_rename = false,
+            lsp_doc_border = false,
+          },
+        })
+      end
     }
   },
 })
